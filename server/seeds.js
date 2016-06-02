6acd99210c762f4f3dd1cbf91696b7f44f078849
John Pinkerton <jpinkerton88@gmail.com>
2016-06-02 18:55:45 -0400

    end seeds

--- server/seeds.js
+++ server/seeds.js
@@ -1,16 +1,16 @@
-import People from "../lib/people";
+// import People from "../lib/people";
 
-if (People.find().count() === 0) {
-  for (let i = 0; i < 100; i++) {
-    People.insert({
-      email: `test${i+1}@test.com`,
-      charged: false,
-      error: false,
-      card: "4111111111111111",
-      month: 12,
-      year: 2020,
-      cvc: 111,
-      zip: "11111",
-    });
-  }
-}
+// if (People.find().count() === 0) {
+//   for (let i = 0; i < 100; i++) {
+//     People.insert({
+//       email: `test${i+1}@test.com`,
+//       charged: false,
+//       error: false,
+//       card: "4111111111111111",
+//       month: 12,
+//       year: 2020,
+//       cvc: 111,
+//       zip: "11111",
+//     });
+//   }
+// }
